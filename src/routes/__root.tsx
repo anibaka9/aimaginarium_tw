8b2e5def8a5ce32d7e30822e7f9f9f9ef69f9ef3
Aleskei Budkin <anibaka9@gmail.com>
2024-04-12 13:03:54 +0600

    added speedinsides and analitycs

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,16 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Toaster />
       <Outlet />
+      <SpeedInsights />
+      <Analytics />
     </>
   ),
 });
