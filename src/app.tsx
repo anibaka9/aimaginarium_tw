fe9ee3ae3bd23ec309d94cc9ab79208e3ca3a736
Aleskei Budkin <anibaka9@gmail.com>
2024-04-12 12:53:58 +0600

    fixed ts errors

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
