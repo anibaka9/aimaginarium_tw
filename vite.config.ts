5051cc8563b2f2027f564b29ca454834c3692dc7
Aleskei Budkin <anibaka9@gmail.com>
2024-04-30 21:50:27 +0600

    connected centry source maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
@@ -5,10 +6,18 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), sentryVitePlugin({
+    org: "anibaka-co",
+    project: "javascript-react"
+  })],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 });
